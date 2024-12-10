96d30618f0751fda8005dc53fbabf5b9dbf51622
Veronicchkamin <veronichka.minchonak@gmail.com>
2024-12-10 17:01:31 +0100

    Homework was done

--- src/Homeworks/MainApp.java
+++ src/Homeworks/MainApp.java
@@ -3,5 +3,27 @@ package Homeworks;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello!");
+
+        int[] mass = {5, 10, 13, 17, 66};
+        int sum = 0;
+        int max = mass[0];
+        int min = mass[0];
+
+        for (int i = 0; i < mass.length; i++) {
+            if (min > mass[i]) {
+                min = mass[i];
+            }
+            if (max < mass[i]) {
+                max = mass[i];
+            }
+
+            sum += mass[i];
+        }
+
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+        System.out.println("Sum: " + sum);
+        System.out.println("Amount of elements " + mass.length);
     }
 }
